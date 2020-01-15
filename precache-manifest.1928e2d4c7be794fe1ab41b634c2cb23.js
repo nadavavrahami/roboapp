8c93fdf37e386905874ca0f9d6b56468f385b2b9
nadavavrahami <nadav170293@gmail.com>
2020-01-15 13:39:18 +0200

    Updates

---
+++ precache-manifest.1928e2d4c7be794fe1ab41b634c2cb23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d5b667d69404cc7841ccaac53e5804",
+    "url": "/roboapp/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/roboapp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "bbd9e2d26ba366c5e59f",
+    "url": "/roboapp/static/css/main.757a4481.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/roboapp/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "bbd9e2d26ba366c5e59f",
+    "url": "/roboapp/static/js/main.d613758e.chunk.js"
+  },
+  {
+    "revision": "d2529f91704a8dad9612",
+    "url": "/roboapp/static/js/runtime-main.648586ab.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
